550d0469481c08a6499791c8efa70d0657674dab
xepiqq <jaydencrowther@gmail.com>
2025-05-16 09:05:02 +0000

    changed metadata name

--- app/layout.jsx
+++ app/layout.jsx
@@ -9,7 +9,7 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Koldinfo",
+  title: "Contacted",
   description: "get leads",
 };
 
